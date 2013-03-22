ec7778c8af1f5c81d5cfe8b6c8bfa4af86202441
Haritas <hari.hasic@gmail.com>
2013-03-22 12:05:06 +0100

    A

---
+++ Maturitetna Naloga/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+
+namespace Maturitetna_Naloga
+{
+    public partial class Form1 : Form
+    {
+        const string quote = "\"";
+        int start,start2;
+        string x;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WebClient client = new WebClient();
+            richTextBox1.Text = client.DownloadString("http://www.sc-celje.si/ek/informacije/Lists/Suplence/AllItems.aspx");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            start = richTextBox1.Text.IndexOf("Summary=" + quote + "Suplence");
+           string x = richTextBox1.Text.Substring(start);
+            richTextBox1.Text = x;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            start = richTextBox1.Text.IndexOf("<TBODY id="+quote+"titl1-1");
+            x = richTextBox1.Text.Substring(start);
+            start = richTextBox1.Text.IndexOf("<TR class=" + quote + quote);
+            x = richTextBox1.Text.Substring(start);
+            richTextBox1.Text = x;
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            start = richTextBox1.Text.IndexOf("</TABLE>");
+            x = richTextBox1.Text.Substring(0, start);
+            richTextBox1.Text = x;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string[] values = richTextBox1.Text.Split(new string[] { "<tr>", "</tr>", "<td>", "</td>" }, StringSplitOptions.RemoveEmptyEntries);
+
+            List<string> list = new List<string>(values);
+
+            string[,] atributes = new string[list.Count, 8];
+
+            for (int i = 1; i < values.Length; i++) // REZANJE IZ VSAKE VRSTICE IZ TABELE
+            {
+                start = list[i].IndexOf("<NOBR>");
+                start2 = list[i].IndexOf("</NOBR>");
+                atributes[i,0] = list[i].Substring(start+6,start2-25); // DATUM
+
+                list[i] = list[i].Substring(start2+31);
+                start2 = list[i].IndexOf("</TD>");
+                atributes[i, 1] = list[i].Substring(0, start2); // IME MANJKAJOČEGA UČITELJA
+
+                list[i] = list[i].Substring(start2 + 24);
+                start2 = list[i].IndexOf("</TD>");
+                atributes[i, 2] = list[i].Substring(0, start2); // IME NADOMESTNEGA UČITELJA
+
+                list[i] = list[i].Substring(start2 + 24);
+                start2 = list[i].IndexOf("</TD>");
+                atributes[i, 3] = list[i].Substring(0, start2); // RAZRED
+
+                list[i] = list[i].Substring(start2 + 24);
+                start2 = list[i].IndexOf("</TD>");
+                atributes[i, 4] = list[i].Substring(0, start2); // UCILNICA
+
+                list[i] = list[i].Substring(start2 + 24);
+                start2 = list[i].IndexOf("</TD>");
+                atributes[i, 5] = list[i].Substring(0, start2); // PREDMET
+
+                list[i] = list[i].Substring(start2 + 24);
+                start2 = list[i].IndexOf("</TD>");
+                atributes[i, 6] = list[i].Substring(0, start2); // VRSTA NADOMESCANJA
+
+                list[i] = list[i].Substring(start2 + 41);
+                start2 = list[i].IndexOf("</DIV>");
+                atributes[i, 7] = list[i].Substring(0, start2); // URA
+            }
+
+            
+
+
+        }
+    }
+}
---
+++ Maturitetna Naloga/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Maturitetna Naloga")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Maturitetna Naloga")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("92e29b5f-019a-4774-9ca2-2930212259cb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
